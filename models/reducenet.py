929104517a79affdadd4c8114dc245e6dadfea12
ohmydroid <110890341+ohmydroid@users.noreply.github.com>
2023-05-28 19:28:33 +0800

    add expansion factor for bottleneck block

--- models/reducenet.py
+++ models/reducenet.py
@@ -10,16 +10,18 @@ def _weights_init(m):
 
 class BasicBlock(nn.Module):
 
-    def __init__(self, in_planes, planes, stride=1,scaler=0.):
+    def __init__(self, in_planes, planes, stride=1,scaler=0.,expansion=1):
+        ## expansion factor allows bottleneck structure for basi block. There exists high dimensional intermediate representation during training, but only low dimensional representation will be left during inference.
+        
         super(BasicBlock, self).__init__()
 
         self.shortcut = True if stride==1 else False
         self.scaler = scaler
 
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(in_planes, planes*expansion, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
 
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv2 = nn.Conv2d(expansion*planes, planes, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
 
