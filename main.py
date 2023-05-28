e2aaaeef38c2d366e0edb8d50cd02983bef93f01
ohmydroid <lq.y573@outlook.com>
2023-05-28 15:05:17 +0800

    delete useless code of main.py

--- main.py
+++ main.py
@@ -90,28 +90,28 @@ if args.dataset == 'cifar10':
 
    # Data
    print('==> Preparing data cifar10')
-   trainset = torchvision.datasets.CIFAR10(root='/home/onedroid/apps/projects/pytorh-vision/data', train=True, download=False, transform=transform_train)
+   trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform_train)
    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=args.workers,pin_memory=True)
 
-   testset = torchvision.datasets.CIFAR10(root='/home/onedroid/apps/projects/pytorh-vision/data', train=False, download=False, transform=transform_test)
+   testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform_test)
    testloader = torch.utils.data.DataLoader(testset, batch_size=512, shuffle=False, num_workers=args.workers,pin_memory=True)
 
 else:
     # Data
    print('==> Preparing data..')
-   trainset = torchvision.datasets.CIFAR100(root='/home/onedroid/apps/projects/pytorh-vision/data', train=True, download=True, transform=transform_train)
+   trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=args.workers)
-   testset = torchvision.datasets.CIFAR100(root='/home/onedroid/apps/projects/pytorh-vision/data', train=False, download=True, transform=transform_test)
+   testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
    testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=args.workers)
 
 
 if args.model == 'reduce20':
    net = reducenet20(num_classes)
-   print('reducenet20 is loaded')
+   print('reducenet20 loaded')
 else:
     net = reducenet56(num_classes)
-    print('reducenet56 is loaded')
-print('num_classes is {}'.format(num_classes))
+    print('reducenet56 loaded')
+
 summary(net, torch.zeros((1, 3, 32, 32)))
 
 #net = torch.compile(net)
